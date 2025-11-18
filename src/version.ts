68cb603561a813e3534bc4feb8e680afea035e23
ibrahimzaink <ibrahimkapoor11@gmail.com>
2025-11-18 15:19:08 +0300

    chore(release): bump app version to 0.1.0

---
+++ src/version.ts
@@ -0,0 +1,5 @@
+// Simple, editable app version file.
+// Update the version here as part of your release process.
+// synchronized with top-level package.json
+export const APP_VERSION = '0.1.0';
+export default APP_VERSION;
